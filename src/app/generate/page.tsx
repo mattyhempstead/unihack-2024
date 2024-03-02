902cc23f09bf75edf4e8d72c1cc2702e88d68a8d
jedwed <83894732+jedwed@users.noreply.github.com>
2024-03-02 23:07:33 +1100

    feature: user selects their mood as the first prompt

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,69 +1,102 @@
-"use client";
+'use client';
 
-import { type FunctionCallHandler } from "ai";
-import { useChat } from "ai/react";
+import React, { useState } from 'react';
+import { type FunctionCallHandler } from 'ai';
+import { useChat } from 'ai/react';
 
-type MemeTemplates = "drake_hotline" | "distracted_boyfriend";
+import { getFunctionName } from '@/meme-gen/schema';
+import { memeTemplates } from '@/meme-gen/memes';
+import Options from '@/components/Options';
 
-type GenerateMemeArgs = {
-  template: MemeTemplates;
-  message: string;
-};
+type MemeGenState = {
+  name: string;
+  props: {
+    [fieldName: string]: string;
+  };
+} | null;
 
-function generateMeme(args: GenerateMemeArgs) {}
+const moods = ['Ecstatic!!!', 'Happy :)', 'Sad :(', 'Depressed ;.;'];
 
-export default function Chat() {
+export default function Generate() {
+  const [state, setState] = React.useState<MemeGenState>(null);
+
   const onFunctionCall: FunctionCallHandler = async (
     chatMessages,
-    functionCall
+    functionCall,
   ) => {
-    console.log("function handler!");
+    console.log('function handler!');
+    console.log(messages);
 
-    if (functionCall.name === "generate_drake_hotline_bling_meme") {
-      const parsedArgs = JSON.parse(functionCall.arguments ?? "");
-      console.log(parsedArgs);
+    memeTemplates.forEach((template) => {
+      if (functionCall.name === getFunctionName(template.name)) {
+        const parsedArgs = JSON.parse(functionCall.arguments ?? '');
+        console.log(parsedArgs);
 
-      // render meme
-    }
+        setState({
+          name: template.name,
+          props: parsedArgs,
+        });
+      }
+    });
 
     return {
       messages: chatMessages,
     };
   };
 
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
+  const { messages, input, handleInputChange, handleSubmit, append } = useChat({
     initialMessages: [],
     experimental_onFunctionCall: onFunctionCall,
   });
 
-  /**
-   *
-   * [
-   * { role: "system", message: "you're a meme generator assistant, your job is to listen to your user's rant and come up with memes"},
-   *  {role: "user", message: "user's input"},
-   * {role: "assistant", message: "assistant's resopnse"},
-   * {role: "function"}
-   * ]
-   *
-   */
+  const memeRenderer = memeTemplates.find(
+    (template) => template.name === state?.name,
+  )?.render;
+
+  const memeJsx =
+    memeRenderer && state?.props ? memeRenderer(state.props) : null;
+
+  const [moodSelected, setMoodSelected] = useState(false);
+  const handleSelectMood = (option: string) => {
+    setMoodSelected(true);
+    append({
+      role: 'system',
+      content: `
+        The assistant asked the question: "How is your day going?".
+        The user responded: "${option}"
+      `,
+    });
+  };
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === "user" ? "User: " : "AI: "}
-          {m.content}
-        </div>
-      ))}
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      {memeJsx}
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+      <div className="whitespace-pre-wrap">Hello, how is your day?</div>
+      <Options
+        options={moods}
+        handleSelect={handleSelectMood}
+        alreadySelected={moodSelected}
+      />
+      {messages
+        .filter((m) => m.role === 'user' || m.role === 'assistant')
+        .map((m) => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            {m.role === 'user' ? 'User: ' : 'AI: '}
+            {m.content}
+          </div>
+        ))}
+      {moodSelected && (
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      )}
     </div>
   );
 }
---
+++ src/components/Options.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Options({
+  options,
+  handleSelect,
+  alreadySelected,
+}: {
+  options: string[];
+  handleSelect: (option: string) => void;
+  alreadySelected: boolean;
+}) {
+  return (
+    <div className="flex gap-3">
+      {options.map((option, idx: number) => (
+        <button
+          key={idx}
+          onClick={() => handleSelect(option)}
+          disabled={alreadySelected}
+          className="border border-lg rounded-lg p-2 hover:bg-gray-50"
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
